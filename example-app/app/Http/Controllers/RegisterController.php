e6375cfd1be24db7079aa86c823c397230dc7385
Aek Sae-khow <saekhow.aek@gmail.com>
2021-01-24 21:32:18 +0700

    Update register page

--- example-app/app/Http/Controllers/RegisterController.php
+++ example-app/app/Http/Controllers/RegisterController.php
@@ -9,4 +9,26 @@ class RegisterController extends Controller
     public function index() {
         return view('auth.register');
     }
+
+    public function store(Request $request) {
+        
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // store user
+        // sign the user in
+        // redirect
+    }
+
 }
